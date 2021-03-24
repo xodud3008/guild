bc776025a3e0cc90a3347e048034ee89dcd3f4b3
xodud3008 <xodud3008@gmail.com>
2021-03-24 17:57:15 +0900

    commit

---
+++ src/test/java/com/guild/withAccountSecurityContextFactory.java
@@ -0,0 +1,33 @@
+package com.guild;
+
+import com.guild.modules.account.AccountService;
+import com.guild.modules.account.form.SignUpForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+@RequiredArgsConstructor
+public class withAccountSecurityContextFactory implements WithSecurityContextFactory<WithAccount> {
+
+    private final AccountService accountService;
+
+    @Override
+    public SecurityContext createSecurityContext(WithAccount withAccount) {
+        String nickname = withAccount.value();
+
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname(nickname);
+        signUpForm.setEmail(nickname + "@gmail.com");
+        signUpForm.setPassword("12345678");
+        accountService.processNewAccount(signUpForm);
+
+        UserDetails principal = accountService.loadUserByUsername(nickname);
+        Authentication  authentication = new UsernamePasswordAuthenticationToken(principal, principal.getPassword(), principal.getAuthorities());
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+        return context;
+    }
+}
